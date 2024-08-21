fa76b48d3cbb2287fb09402717748bdc48109010
Bekzhan <jer710@gmail.com>
2024-08-21 17:44:42 -0400

    update

--- src/test/java/Runners/CucumberRunner.java
+++ src/test/java/Runners/CucumberRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = ".//Feature/login.feature",
+        features = "src/test/resources/features/goldBarGame.feature",
         glue = "stepDefinitions",
         dryRun = false,
         plugin = {"pretty", "html:test-output"}
